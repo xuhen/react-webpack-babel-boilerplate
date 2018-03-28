a64c1956ece653e92531aea906b2bf2d2bee2217
Edward xu <xuheng@goodsfans.com>
2018-03-28 16:00:17 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app/App.jsx';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
